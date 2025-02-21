cb7e372f1a5a15ee9645b8a1ae13ba531571a4c5
NellyMakena <nellysolomonmakena3@gmail.com>
2025-02-21 12:14:09 +0300

    nav

--- FitUs/settings.py
+++ FitUs/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for FitUs project.
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'FitUs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
