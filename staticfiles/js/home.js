fbbd2641798d1c8bb667cf3dfa6970685fcefe89
NellyMakena <nellysolomonmakena3@gmail.com>
2025-04-11 20:43:36 +0300

    nav-fixed

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -1,19 +1,6 @@
-let menu = document.querySelector ('#menu-icon')
-let navbar = document.querySelector ('.navbar')
-
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open')
-};
-
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('open')
-};
-
-
-
+function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("active");
+}
 document.addEventListener("DOMContentLoaded", function () {
     const testimonials = [
         {
