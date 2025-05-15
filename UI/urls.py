0aa3ed9f6018c873fa496541478f1f13623867d6
NellyMakena <nellysolomonmakena3@gmail.com>
2025-05-15 16:48:20 +0300

    getpage

--- UI/urls.py
+++ UI/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view,training_view,nutrition_view,help_view,shop_view
+from .views import home_view,training_view,nutrition_view,help_view,shop_view,get_view
 
 app_name = 'UI'
 
@@ -9,5 +9,5 @@ urlpatterns = [
     path('nutrition/',nutrition_view, name='nutrition'),
     path('help/',help_view,name='help'),
     path('shop/',shop_view,name='shop'),
-
+    path('get/',get_view,name='get'),
 ]
