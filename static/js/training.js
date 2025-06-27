921d074c8c82adc701c4db19a88e68c688898238
NellyMakena <nellysolomonmakena3@gmail.com>
2025-06-27 13:49:49 +0300

    training

--- static/js/training.js
+++ static/js/training.js
@@ -1,27 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const textSections = document.querySelectorAll(".app-text");
-    const phoneImage = document.getElementById("app-phone-image");
 
-    function updatePhoneImage() {
-        let current = null;
-        textSections.forEach((section) => {
-            const rect = section.getBoundingClientRect();
-            if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-                current = section;
-            }
-        });
-
-        if (current) {
-            const newImage = current.getAttribute("data-image");
-            if (phoneImage.src !== newImage) {
-                phoneImage.src = newImage;
-            }
-        }
-    }
-
-    window.addEventListener("scroll", updatePhoneImage);
-    updatePhoneImage(); // first image
-});
 
 document.querySelectorAll(".vedio-item").forEach(item => {
     item.addEventListener("click", () => {
